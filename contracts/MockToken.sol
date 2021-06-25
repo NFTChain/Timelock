06908e105ab1ef9a583a5c41c3d341052b34f1a9
Liam Sutton <devliam90@gmail.com>
2021-06-25 16:49:23 +0100

    test pass

--- contracts/MockToken.sol
+++ contracts/MockToken.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.8.4;
 
-import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MockToken is ERC20 {
-  constructor() ERC20('Mock Token', 'MTK') {
-    _mint(msg.sender, 1 * 10 ** 18);
-  }
+    constructor() ERC20("Mock Token", "MTK") {
+        _mint(msg.sender, 1 * 10**18);
+    }
+}
